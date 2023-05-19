173192d17592743840e5dc74d4c891ab15a0dce2
kirub <kirub@MyHome.attlocal.net>
2023-05-19 12:33:35 -0500

    Recent Allure Result generated

--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -4,9 +4,9 @@ import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.testng.CucumberOptions;
 import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 	
 @RunWith(Cucumber.class) //Junit execution
 
